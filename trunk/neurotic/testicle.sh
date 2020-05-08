1656ec4a3c3fd14ee8d2c32bc2d2068219a40881
oiubrab <oiubrab@87bd508b-4cdc-4f76-ac0e-c41c8b87a72d>
2020-05-08 08:45:57 +0000

    moving folder to new system

--- trunk/neurotic/testicle.sh
+++ trunk/neurotic/testicle.sh
@@ -3,5 +3,5 @@
 if [ -f 'megalomaniac' ]; then rm megalomaniac; fi
 gfortran discrete_flesh.f95 network.f95 -o megalomaniac
 reset
-./megalomaniac closed 50000 30 60 0 yes
+./megalomaniac open 50 10 10 0 debug
 rm megalomaniac
