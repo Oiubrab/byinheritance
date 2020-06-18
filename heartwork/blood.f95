0e67716cf18994c92e3efac43e9adef5826efd86
Adrian <52502530+Oiubrab@users.noreply.github.com>
2020-06-18 20:57:55 +0000

    files compile. An illegal memory acces is cropping up when I run the thing. Attempting to recourse by exploding

--- heartwork/blood.f95
+++ heartwork/blood.f95
@@ -1,13 +1,11 @@
 program bloody
 use flesh
 use cudafor
+use curand
 implicit none
 
 !eyes, ears and nose are data inputs to be determined (RANDOM_NUMBER for now)
 !time_interval to be used to time epoch to compute information degradation, total_time just to store total time of program execution
-
-!fundamental parameters
-real,parameter :: pi=4*asin(1./sqrt(2.))
 
 !input variables
 character(len=10000) :: maxim_column_cha,maxim_row_cha,printed,multiplier_scaling_cha
@@ -17,20 +15,19 @@ real ::  time_interval, start, finish, start_interval, finish_interval
 
 !main brain objects
 integer :: maxim_column,maxim_row
-real,allocatable :: blood(:,:,:)
+real,allocatable :: blood(:,:,:),blood_transition(:,:,:,:)
 
 !network objects
 integer,allocatable :: brain(:,:,:)
 integer :: active_data,grave
+real,allocatable :: hope(:,:,:)
 
 !debugging objects
 integer :: x
 
 !incrementation objects
 integer :: epoch, j,i,z, f,u,k, s,a,c, pos_hold
-integer,allocatable :: matrix_pos(:)
-
-real :: hope, fear, fate, transition, distil,multiplier_scaling
+real :: fate, transition, distil,multiplier_scaling
 
 !printing objects
 integer :: print_length=7
@@ -40,12 +37,12 @@ character(len=:),allocatable :: print_row
 !file checking
 logical :: file_exists
 
-!gpu translation variables
+!translation variables
 type(dim3) :: thread_per_block,block_per_grid
-real,allocatable,device :: blood_device(:,:,:)
+real,allocatable,device :: hope_device(:,:,:)
+real,allocatable,device :: blood_device(:,:,:),blood_transition_device(:,:,:,:)
 integer,allocatable,device :: brain_device(:,:,:)
-integer,allocatable,device :: matrix_pos_device(:)
-
+integer,allocatable :: master(:,:)
 
 
 
@@ -56,7 +53,7 @@ call random_seed()
 !prepare command line options
 IF(COMMAND_ARGUMENT_COUNT().NE.4)THEN
 	WRITE(*,*)'Execute program by format:'
-	WRITE(*,*)'./program epoch_number maximum_columns maximum_rows scaling printed'
+	WRITE(*,*)'./program maximum_columns maximum_rows scaling printed'
 	WRITE(*,*)'printed: yes no'
 	STOP
 ENDIF
@@ -72,7 +69,7 @@ READ(maxim_row_cha,*)maxim_row
 
 if ((printed/='no') .and. (printed/='yes') .and. (printed/='debug')) then
 	WRITE(*,*)'Execute program by format:'
-	WRITE(*,*)'./program epoch_number maximum_columns, maximum_rows scaling printed'
+	WRITE(*,*)'./program maximum_columns maximum_rows scaling printed'
 	WRITE(*,*)'printed: yes no'
 	stop
 end if
@@ -80,8 +77,7 @@ end if
 thread_per_block%x=1024
 thread_per_block%y=1
 thread_per_block%z=1
-block_per_grid=dim3(ceiling(float(maxim_row*maxim_column)/thread_per_block%x),&
-	ceiling(float(maxim_column)/thread_per_block%y),ceiling(float(maxim_row)/thread_per_block%z))
+block_per_grid=dim3(ceiling(float(maxim_column)/thread_per_block%x),ceiling(float(maxim_row)/thread_per_block%y),1)
 
 
 
@@ -95,17 +91,20 @@ block_per_grid=dim3(ceiling(float(maxim_row*maxim_column)/thread_per_block%x),&
 call CPU_Time(start)
 
 
-!the first dimension is the number of levels (rows)
-!the second dimension is the number of possible neurons in each level (column)
-!the third dimension holds the data, (j,i,((j-1)*maxim_column+i)), and the weights, (j,i,z) for ((j-1)*maxim_column+i)/=z, as they relate to each of the other neurons
+!these are the allocations of all the arrays in the step
 allocate(blood(maxim_row*maxim_column,maxim_column,maxim_row))
 allocate(blood_device(maxim_row*maxim_column,maxim_column,maxim_row))
-allocate(brain(1:maxim_column*maxim_row+2*(maxim_column+maxim_row)-4,1:maxim_column,1:maxim_row))
-allocate(brain_device(1:maxim_column*maxim_row+2*(maxim_column+maxim_row)-4,1:maxim_column,1:maxim_row))
 
-!initialise the randomised position marker arrays
-allocate(matrix_pos(1:size(blood(:,1,1))))
-allocate(matrix_pos_device(1:size(blood(:,1,1))))
+allocate(blood_transition(2,maxim_row*maxim_column,maxim_column,maxim_row))
+allocate(blood_transition_device(2,maxim_row*maxim_column,maxim_column,maxim_row))
+
+allocate(master(maxim_row*maxim_column,2))
+
+allocate(hope(maxim_row*maxim_column,maxim_column,maxim_row))
+allocate(hope_device(maxim_row*maxim_column,maxim_column,maxim_row))
+
+allocate(brain(maxim_column*maxim_row+2*(maxim_column+maxim_row)-4,maxim_column,maxim_row))
+allocate(brain_device(maxim_column*maxim_row+2*(maxim_column+maxim_row)-4,maxim_column,maxim_row))
 
 !initialise printer
 allocate(character(maxim_column*print_length+7) :: print_row)
@@ -184,43 +183,22 @@ else
 	epoch=epoch+1
 	close(1)	
 
-	!affect the blood with the brain multipliers
+	!do all the neuron stuff
+	call random_number(hope)
+	hope_device=hope
+	blood_device=blood
 	brain_device=brain
-	blood_device=blood
-	call electroviolence<<<block_per_grid, thread_per_block>>>(brain_device,blood_device,multiplier_scaling)
-	blood=blood_device
-
-	!simple tester
-	do x=1,6
-		if (epoch>(10*x)) then
-			blood(self_pos(maxim_row,1,maxim_column),1,maxim_row)=blood(self_pos(maxim_row,1,maxim_column),1,maxim_row)+0.00001*(10**x)
-			blood(self_pos(maxim_row,maxim_column/2,maxim_column),maxim_column/2,maxim_row)=&
-				blood(self_pos(maxim_row,maxim_column/2,maxim_column),maxim_column/2,maxim_row)+0.00001*(10**x)
-			blood(self_pos(maxim_row,maxim_column,maxim_column),maxim_column,maxim_row)=&
-				blood(self_pos(maxim_row,maxim_column,maxim_column),maxim_column,maxim_row)+0.00001*(10**x)
-		end if
-	end do
-
-	!record the start time of the epoch
-	call CPU_Time(start_interval)
-
-	!the randomised loop initialiser - ensures data transition is not positionally dependant
-	call randomised_list(matrix_pos)
-
+	blood_transition_device=blood_transition
+	print*,"awaken"
+	call neuron_pre_fire<<<block_per_grid,thread_per_block>>>(hope_device,blood_device,blood_transition_device,brain_device,multiplier_scaling)
+	hope_device=hope
+	print*,"asleep"
 	
-	blood_device=blood
-	matrix_pos_device=matrix_pos
-	
-	!this is the brain neuron action loop (main loop)
-	!set the prospective transitionn value random multipliers
-	call RANDOM_NUMBER(hope)
-	call RANDOM_NUMBER(fear)
-	call RANDOM_NUMBER(fate)
-	call neuron_fire<<<block_per_grid, thread_per_block>>>(blood_device,matrix_pos_device,hope,fear,fate)
-	
-	blood=blood_device
-	matrix_pos=matrix_pos_device
+	blood_transition=blood_transition_device
 
+	call randomised_sex(master)
+
+	call random_neuron_fire(blood,blood_transition,master)
 
 	!print the brain
 	if ((printed=="yes") .or. (printed=='debug')) then
@@ -228,7 +206,7 @@ else
 		do s=1,size(blood(1,1,:))
 			do a=1,size(blood(1,:,1))
 
-				write(data_cha,"(F7.3)")blood(self_pos(s,a,maxim_column),a,s)
+				write(data_cha,"(F8.3)")blood(self_pos(s,a,maxim_column),a,s)
 				print_row(a*print_length:a*print_length+7)=data_cha
 				
 			end do
@@ -238,17 +216,6 @@ else
 		print*," "
 	
 	end if
-
-	!enact time penalty on each neuron
-	!this is a key part of the system's learning power
-	!this ensures against runaway neuron growth and also limits growth of the brain past a point where neuron action takes too long
-	do z=1,size(blood(1,1,:))
-		do i=1,size(blood(1,:,1))
-			do j=1,size(blood(:,1,1))
-				blood(j,i,z)=blood(j,i,z)*(1-time_interval)
-			end do
-		end do
-	end do
 	
 end if
 
