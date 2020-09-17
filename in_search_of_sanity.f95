a1d6e88f678e75d33dc56515429d00ddaa476018
Adrian <52502530+Oiubrab@users.noreply.github.com>
2020-09-17 15:08:30 +0000

    added response network that takes data outgoing from bottom of brain and stores it

--- in_search_of_sanity.f95
+++ in_search_of_sanity.f95
@@ -8,10 +8,11 @@ integer,parameter :: info_ports=66 !first 64 are weights, 65 the origin address 
 integer :: rows=10, columns=15
 integer, allocatable :: brain(:,:,:), blood(:,:,:)
 
-!sensing setup
-integer, allocatable :: vision(:)
-integer,parameter :: data_rate=500
+!sensing and response setup
+integer, allocatable :: vision(:), response(:)
+integer,parameter :: data_rate=200
 real :: look !test variable for randomisiong vision array
+character(len=6) :: opener="bottom"
 
 !selecting and moving
 integer :: row_number, column_number, row_number_2, column_number_2, info_number, row_random_number, column_random_number
@@ -22,7 +23,7 @@ integer :: moves=0, epoch, epoch_total=1000
 integer :: use_reward=30, blood_gradient=30
 
 !timing
-real :: start, finish, delay_time=0.5
+real :: start, finish, delay_time=0.05
 
 !printing
 integer,parameter :: individual_width=2, separation_space=10
@@ -37,7 +38,8 @@ character(len=:),allocatable :: print_row
 call CPU_Time(start)
 
 !allocation block
-allocate(vision(columns)) !allocate the variable for input into the network
+allocate(vision(columns)) !allocate the array for input into the network, currently on top
+allocate(response(columns)) !allocate the array for output from the network, currently on bottom
 allocate(brain(info_ports,columns,rows)) !allocate the network variable
 allocate(blood(info_ports,columns,rows)) !allocate the reward variable
 allocate(column_random(columns)) !allocate the column selection randomiser (randomised at main loop start)
@@ -45,10 +47,7 @@ allocate(row_random(rows)) !allocate the row selection randomiser (randomised at
 
 
 !initialise the network
-call initialiser(brain,blood,vision)
-!do column_number_2=1,columns
-!	print*,blood(info_ports,column_number_2,rows)
-!end do
+call initialiser(brain,blood,vision,response,opener)
 
 !test injection - an origin must accompany the data
 vision(2)=1 !change this to detect the food position when I attach this to the game
@@ -62,16 +61,16 @@ end do
 
 
 print*,"Brain moves: 0 Epoch: 0"
-call print_network(brain,blood,vision)
+call print_network(brain,blood,vision,response)
 
 !this is the new song
 do epoch=1,epoch_total
 
-	!test injection - an origin must accompany the data
+	!injection from vision into brain
 	if (mod(epoch,data_rate)==0) then
 		do column_number=1,columns
 			if (vision(column_number)==1) then	
-				brain(info_ports-1,column_number,1)=2
+				brain(info_ports-1,column_number,1)=2 !an origin must accompany the data
 				brain(info_ports,column_number,1)=1
 			end if
 		end do
@@ -80,9 +79,9 @@ do epoch=1,epoch_total
 	!test - randomise vision
 	vision=0
 	call random_number(look)
-	vision(int(look*float(columns)))=1
+	vision(int(look*float(columns))+1)=1
 	call random_number(look)
-	vision(int(look*float(columns)))=1
+	vision(int(look*float(columns))+1)=1
 
 	!first, randomise random column and row lists
 	call randomised_list(row_random)
@@ -112,7 +111,7 @@ do epoch=1,epoch_total
 			
 				!here is the important subroutine call that moves the data depending on how fat the neuron is 
 				!individual data may move several times each loop. Loop these loops for a truly random movement (feature, not bug) 
-				call selector(blood,brain,column_random_number,row_random_number,use_reward)
+				call selector(blood,brain,column_random_number,row_random_number,use_reward,response)
 				
 				!the choosing weights reduce by one if they are not used and increase by use_reward-1 if they are used
 				!this is done by subtracting one from all weights and adding use_reward to weights that are used
@@ -132,7 +131,7 @@ do epoch=1,epoch_total
 				
 				!print each step
 				print'(A15,I0,A8,I0)',"Brain moves: ",moves,"Epoch: ",epoch
-				call print_network(brain,blood,vision)
+				call print_network(brain,blood,vision,response)
 				
 			end if
 			
