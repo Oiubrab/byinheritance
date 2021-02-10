6e641164b2c838a4c6110c0a3ac1923ca7d75d20
Adrian <52502530+Oiubrab@users.noreply.github.com>
2021-02-10 12:34:06 +0000

    The networks 'motivate' and 'think' are now connected through the 'oddsey' output, which is based on the 'motivate' output

--- welcome_to_dying.f95
+++ welcome_to_dying.f95
@@ -936,22 +936,37 @@ end subroutine initialiser
 
 
 !This subroutine takes the neurochem matricies saved in each network and the output of the motivate network and applies a scaling factor to the weights of each network
-subroutine animus(think,motivate,oddyseus)
+subroutine animus(meaning,oddyseus)
 
-	type(mind) :: think, motivate
+	type(mind) :: meaning
 	integer :: oddyseus, row, column, to, from, ladder
-	
+
 	!add to weights based on the neurochem at that node and the scaling 
-	do row=1,size(think%brain_weight(1,1,1,:))
-		do column=1,size(think%brain_weight(1,1,:,1))
-			do ladder=1,size(think%neurochem(1,:,1,1))
-				
+	do row=1,size(meaning%brain_weight(1,1,1,:))
+		do column=1,size(meaning%brain_weight(1,1,:,1))
+			do ladder=1,size(meaning%neurochem(1,:,1,1))
+			
 				!for each rung in the neurochem ladder, establish which weight is to be altered
-				from=think%neurochem(1,ladder,column,row)
-				to=think%neurochem(2,ladder,column,row)
-				!add the weight
-				think%brain_weight(to,from,column,row)=think%brain_weight(to,from,column,row)+(size(think%neurochem(1,:,1,1))+1-ladder)*oddyseus
+				from=meaning%neurochem(1,ladder,column,row)
+				to=meaning%neurochem(2,ladder,column,row)
+				
+				!exclude ladder rungs that haven't been set yet
+				if ((to/=0) .and. (from/=0)) then 
+					!exclude intentionally zeroed out weights
+					if (meaning%brain_weight(to,from,column,row)/=0.0) then
 					
+						!add the weight
+						meaning%brain_weight(to,from,column,row)=meaning%brain_weight(to,from,column,row)+&
+							(size(meaning%neurochem(1,:,1,1))+1-ladder)*oddyseus
+							
+						!if weight manipulation reduces weight below 1.0, make it 1.0
+						if (meaning%brain_weight(to,from,column,row)<1.0) then
+							meaning%brain_weight(to,from,column,row)=1.0
+						end if
+						 
+					end if
+				end if	
+				
 			end do
 		end do
 	end do
